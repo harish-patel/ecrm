979cd7ed3ab47849c706e49297e1d5145be4e21b
Harish Patel <harish.patel@vitruviantech.com>
2013-03-18 12:24:16 +0530

    deleted unwanted modules

--- custom/modules/ed_Employee/Ext/Language/it_it.lang.ext.php
+++
@@ -1,9 +0,0 @@
-<?php 
- //WARNING: The contents of this file are auto-generated
-
-
-//THIS FILE IS AUTO GENERATED, DO NOT MODIFY
-$mod_strings['LBL_ED_DEPARTMENT_ED_EMPLOYEE_FROM_ED_DEPARTMENT_TITLE'] = 'Department';
-$mod_strings['LBL_ED_EMPLOYEE_ED_DEPARTMENT_FROM_ED_DEPARTMENT_TITLE'] = 'Department';
-
-?>
